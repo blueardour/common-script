ee020620745af4dda58b7c96e3b0988813ac0600
anton <anton>
2019-11-08 15:24:03 +1030

    fix bugs

--- run-acl-demo.sh
+++ run-acl-demo.sh
@@ -1,18 +1,22 @@
 
 remote=/data/local/tmp/acl
 target=neoncl_convolution
+if [ "$devices" != "" ]; then device="-s $devices"; fi
+#echo "adb $device shell mkdir -p $remote"
 
 if [ "$1" == "push" ]; then
   cd /workspace/git/ComputeLibrary/build/examples/
-  adb shell mkdir -p $remote
-  adb push $target $remote
-  adb shell chmod 777 -R $remote/
+  adb $device shell mkdir -p $remote
+  adb $device push $target $remote
+  adb $device shell chmod 777 -R $remote/
   cd -
 elif [ "$1" == "run" ]; then
-  adb shell $remote/$target
-  adb shell ls -lh $remote
-  adb shell /data/local/tmp/ai-benchmark/diff $remote/cpu.txt $remote/gpu.txt
+  adb $device shell $remote/$target
+  #adb shell ls -lh $remote
+  #adb shell /data/local/tmp/ai-benchmark/diff $remote/cpu.txt $remote/gpu.txt
   #adb pull $remote
+elif [ "$1" == "parse" ]; then
+  grep 'time cost for execution gpu kernel' $2 | awk -F' ' '{ print $7 }'
 else
   echo "$0 push | run"
 fi
